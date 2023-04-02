463e3f98a888ecc3949fbccacb586dc71f77c770
Amogh Ghadge <amoghghadge22@gmail.com>
2023-04-01 21:40:40 -0400

    added functionality to change data range, added chart titles

--- indicators-visualizer/src/App.js
+++ indicators-visualizer/src/App.js
@@ -6,12 +6,14 @@ import CircularProgress from '@mui/joy/CircularProgress';
 
 function App() {
 
+  const [isLoading, setLoading] = useState(true);
   const allData = useMemo(() => [], []);
   const [data, setData] = useState([]);
-  const [isLoading, setLoading] = useState(true);
+  const [dataIndex, setDataIndex] = useState(0);
   const [minValue, setMinValue] = useState(0);
   const [maxValue, setMaxValue] = useState(0);
   const [buffer, setBuffer] = useState(0.05);
+  const [chartTitle, setChartTitle] = useState("");
 
   useEffect(() => {
     console.log("App loading");
@@ -38,6 +40,137 @@ function App() {
     // eslint-disable-next-line
   }, [data])
 
+  const handleData1Click = () => {
+    setBuffer(0.1);
+    setChartTitle("GDP (Max)");
+    setData(allData[0]);
+    setDataIndex(0);
+  };
+
+  const handleData2Click = () => {
+    setBuffer(0.05);
+    setChartTitle("Inflation (Max)");
+    setData(allData[1]);
+    setDataIndex(1);
+  };
+
+  const handleData3Click = () => {
+    setBuffer(0.01);
+    setChartTitle("Unemployment (Max)");
+    setData(allData[2]);
+    setDataIndex(2);
+  };
+
+  const handleData4Click = () => {
+    setBuffer(0.2)
+    setChartTitle("S&P 500 (Max)");
+    setData(allData[3]);
+    setDataIndex(3);
+  };
+
+  const handleData5Click = () => {
+    setBuffer(0.01);
+    setChartTitle("Federal Interest (Max)");
+    setData(allData[4]);
+    setDataIndex(4);
+  };
+
+  const oneYearRange = () => {
+    let length = allData[dataIndex].length;
+    if (dataIndex === 0) {
+      setBuffer(0.1);
+      setChartTitle("GDP (1y)");
+      setData(allData[dataIndex].slice(length-4));
+    } else if (dataIndex === 1) {
+      setBuffer(0.05);
+      setChartTitle("Inflation (1y)");
+      setData(allData[dataIndex].slice(length-12));
+    } else if (dataIndex === 2) {
+      setBuffer(0.01)
+      setChartTitle("Unemployment (1y)");
+      setData(allData[dataIndex].slice(length-12));
+    } else if (dataIndex === 3) {
+      setBuffer(0.2)
+      setChartTitle("S&P 500 (1y)");
+      setData(allData[dataIndex].slice(length-365));
+    } else if (dataIndex === 4) {
+      setBuffer(0.01);
+      setChartTitle("Federal Interest (1y)");
+      setData(allData[dataIndex].slice(length-12));
+    }
+  }
+
+  const fiveYearRange = () => {
+    let length = allData[dataIndex].length;
+    if (dataIndex === 0) {
+      setBuffer(0.1);
+      setChartTitle("GDP (5y)");
+      setData(allData[dataIndex].slice(length-20));
+    } else if (dataIndex === 1) {
+      setBuffer(0.05);
+      setChartTitle("Inflation (5y)");
+      setData(allData[dataIndex].slice(length-60));
+    } else if (dataIndex === 2) {
+      setBuffer(0.01);
+      setChartTitle("Unemployment (5y)");
+      setData(allData[dataIndex].slice(length-60));
+    } else if (dataIndex === 3) {
+      setBuffer(0.2);
+      setChartTitle("S&P 500 (5y)");
+      setData(allData[dataIndex].slice(length-1825));
+    } else if (dataIndex === 4) {
+      setBuffer(0.01);
+      setChartTitle("Federal Interest (5y)");
+      setData(allData[dataIndex].slice(length-60));
+    }
+  }
+
+  const tenYearRange = () => {
+    let length = allData[dataIndex].length;
+    if (dataIndex === 0) {
+      setBuffer(0.1);
+      setChartTitle("GDP (10y)");
+      setData(allData[dataIndex].slice(length-40));
+    } else if (dataIndex === 1) {
+      setBuffer(0.05);
+      setChartTitle("Inflation (10y)");
+      setData(allData[dataIndex].slice(length-120));
+    } else if (dataIndex === 2) {
+      setBuffer(0.01);
+      setChartTitle("Unemployment (10y)");
+      setData(allData[dataIndex].slice(length-120));
+    } else if (dataIndex === 3) {
+      setBuffer(0.2);
+      setChartTitle("S&P 500 (10y)");
+      setData(allData[dataIndex]);
+    } else if (dataIndex === 4) {
+      setBuffer(0.01);
+      setChartTitle("Federal Interest (10y)");
+      setData(allData[dataIndex].slice(length-120));
+    }
+  }
+
+  const maxRange = () => {
+    if (dataIndex === 0) {
+      setBuffer(0.1);
+      setChartTitle("GDP (Max)");
+    } else if (dataIndex === 1) {
+      setBuffer(0.05);
+      setChartTitle("Inflation (Max)");
+    } else if (dataIndex === 2) {
+      setBuffer(0.01);
+      setChartTitle("Unemployment (Max)");
+    } else if (dataIndex === 3) {
+      setBuffer(0.2);
+      setChartTitle("S&P 500 (Max)");
+    } else if (dataIndex === 4) {
+      setBuffer(0.01);
+      setChartTitle("Federal Interest (Max)");
+    }
+
+    setData(allData[dataIndex]);
+  }
+
   if (isLoading) {
     return (<div className="App" style={{ justifyContent: 'center' }}><CircularProgress size="lg"></CircularProgress></div>)
   }
@@ -49,18 +182,18 @@ function App() {
       <div className="Main-content">
         <div className="Sidebar">
           <p style={{ marginTop: 0, marginBottom: 0 }}>Economic Indicators</p>
-          <Button onClick={() => setData(allData[0])}>GDP</Button>
-          <Button onClick={() => setData(allData[1])}>Inflation</Button>
-          <Button onClick={() => setData(allData[2])}>Unemployment</Button>
-          <Button onClick={() => setData(allData[3])}>S&P 500</Button>
-          <Button onClick={() => setData(allData[4])}>Federal Interest</Button>
+          <Button onClick={handleData1Click}>GDP</Button>
+          <Button onClick={handleData2Click}>Inflation</Button>
+          <Button onClick={handleData3Click}>Unemployment</Button>
+          <Button onClick={handleData4Click}>S&P 500</Button>
+          <Button onClick={handleData5Click}>Federal Interest</Button>
         </div>
         <div className="Main">
           <div className="Data-selector">
-            <Button variant="outlined" size="sm">1Y</Button>
-            <Button variant="outlined" size="sm">5Y</Button>
-            <Button variant="outlined" size="sm">10Y</Button>
-            <Button variant="outlined" size="sm">Max</Button>
+            <Button variant="outlined" size="sm" onClick={oneYearRange}>1y</Button>
+            <Button variant="outlined" size="sm" onClick={fiveYearRange}>5y</Button>
+            <Button variant="outlined" size="sm" onClick={tenYearRange}>10y</Button>
+            <Button variant="outlined" size="sm" onClick={maxRange}>Max</Button>
           </div>
           <LineChart width={1000} height={600}>
             <CartesianGrid strokeDasharray="3 3" />
@@ -68,7 +201,7 @@ function App() {
             <YAxis domain={[minValue, maxValue]} interval="preserveStartEnd" tickCount={10} tick={{ fontSize: 12 }} />
             <Tooltip />
             <Legend />
-            <Line type="monotsone" dataKey="value" data={data} stroke="#8884d8" tick={{ fontSize: 12 }} />
+            <Line name={chartTitle} type="monotsone" dataKey="value" data={data} stroke="#0A6ADD" tick={{ fontSize: 12 }} />
           </LineChart>
         </div>
       </div>
@@ -83,8 +216,8 @@ function App() {
       }
     };
 
-    // const proxy = 'https://proxy.cors.sh/';
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const proxy = 'https://proxy.cors.sh/';
+    // const proxy = 'https://cors-anywhere.herokuapp.com/';
     const requestUrl = `${proxy}https://api.stlouisfed.org/fred/series/observations?series_id=${series_id}&api_key=fcdeab3eee29ac18a7152128f5e48673&file_type=json`;
 
     const response = await fetch(requestUrl, requestOptions);
@@ -97,7 +230,7 @@ function App() {
     }
 
     if (series_id === "FEDFUNDS") {
-      setData(allData[0]);
+      handleData1Click();
       setLoading(false);
     }
 
